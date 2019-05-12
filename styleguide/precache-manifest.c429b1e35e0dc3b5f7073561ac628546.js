ed0c5870cdbf533dcd857f70b5e53b68292ba2c0
savka2017 <savka.ai2015@gmail.com>
2019-05-12 20:49:34 +0300

    add styleguide

---
+++ styleguide/precache-manifest.c429b1e35e0dc3b5f7073561ac628546.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b637d69839bfe716772c",
+    "url": "build/2.b637d698.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "aa957d691de0aa523f8f",
+    "url": "build/main.aa957d69.js"
+  },
+  {
+    "revision": "6f6bb2137c7deac5217b2f6947f11ff9",
+    "url": "index.html"
+  }
+]);
