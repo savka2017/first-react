131844d9b4cd6bb98b52c8e30a6689fae77f0d31
savka2017 <savka.ai2015@gmail.com>
2019-05-12 20:30:42 +0300

    Updates

---
+++ precache-manifest.aef6fd4e99bd370791306ee70ebe709d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1e7b1346653e7958bceeb797889b24",
+    "url": "/first-react/index.html"
+  },
+  {
+    "revision": "21bcdf0dc3160d2584c4",
+    "url": "/first-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/first-react/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "21bcdf0dc3160d2584c4",
+    "url": "/first-react/static/js/main.2dae3238.chunk.js"
+  },
+  {
+    "revision": "c341207d301dab572c5e",
+    "url": "/first-react/static/js/runtime~main.819d487f.js"
+  }
+]);
